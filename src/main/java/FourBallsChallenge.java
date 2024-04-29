f79eb0e88e681476f7a9e8525e446b8030a41952
siva010928 <siva010928@gmail.com>
2024-04-29 19:30:45 +0530

    Implemented Four Balls Challenge using Object Oriented Programming.

--- src/main/java/FourBallsChallenge.java
+++ src/main/java/FourBallsChallenge.java
@@ -1,7 +1,37 @@
 import processing.core.PApplet;
 
 public class FourBallsChallenge extends PApplet {
-    int ball1XPos, ball2XPos, ball3XPos, ball4XPos = 0;
+
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+    public static final int BallHeight = 500;
+
+    static class Ball {
+        private final PApplet sketch;
+        private int xPos;
+        private final float yPos;
+        private final int speed;
+        private final int diameter = 15;
+
+        public Ball(PApplet sketch, float yPos, int speed) {
+            this.sketch = sketch;
+            this.xPos = 0;
+            this.yPos = yPos;
+            this.speed = speed;
+        }
+
+        public void move() {
+            xPos += speed;
+        }
+
+        public void display() {
+            sketch.ellipse(xPos, yPos, diameter, diameter);
+        }
+    }
+
+
+    Ball[] balls = new Ball[4];
+
     public static void main(String[] args) {
         PApplet.main("FourBallsChallenge", args);
     }
@@ -9,19 +39,24 @@ public class FourBallsChallenge extends PApplet {
     @Override
     public void settings() {
         super.settings();
-        size(640, 480);
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+
+        for (int i = 0; i < balls.length; i++) {
+            int yPos = (i + 1) * BallHeight / 5;
+            int speed = i + 1;
+            balls[i] = new Ball(this, yPos, speed);
+        }
     }
 
     @Override
     public void draw() {
-        ellipse(ball1XPos, (float) 1 /5 * 500, 15, 15);
-        ellipse(ball2XPos, (float) 2 /5 * 500, 15, 15);
-        ellipse(ball3XPos, (float) 3 /5 * 500, 15, 15);
-        ellipse(ball4XPos, (float) 4 /5 * 500, 15, 15);
-        ball1XPos += 1;
-        ball2XPos += 2;
-        ball3XPos += 3;
-        ball4XPos += 4;
+        for (Ball ball : balls) {
+            ball.move();
+            ball.display();
+        }
     }
-
 }
